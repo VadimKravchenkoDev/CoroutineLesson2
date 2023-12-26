9db463cf133954f652e295426af7924aee610b5d
Arhizmei <arhizmei@gmail.com>
2023-12-26 16:19:44 +0200

    flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 package org.example
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    
+
 }
+fun <T> Flow<T>.unique():Flow<T>{
+    return  flow {
+        var lastValue: Any? = NoValue
+        collect{value: T ->
+            if (lastValue != value){
+                lastValue = value
+                emit(value)
+            }
+        }
+    }
+}
+
+private object NoValue
